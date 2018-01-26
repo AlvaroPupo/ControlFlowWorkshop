e8dc7474ab45ffc75c6ef0f546cb3714e3ef31e2
AlvaroPupo <juanhernandezpupo@gmail.com>
2018-01-26 15:11:25 -0500

    Proyecto terminado

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,40 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+//while Loops
+//        boolean doWhileCondition = false;
+//        int count = 0;
+//
+//        do {
+//            System.out.println("our while condition is true. Adding 1 to our count");
+//                count = count + 1;
+//                if(count == 3) {
+//                    System.out.println("our count is at 3");
+//                    doWhileCondition = false;
+//                }
+//        } while (doWhileCondition);
+////
+////  For loops
+//        for(int i = 0; i <= 5; i++) {
+//            System.out.println(i);
+//        }
+//
+//       Foreach Loops
+        ArrayList<String> fruit = new ArrayList<String>();
+        fruit.add("Apple");
+        fruit.add("Banana");
+        fruit.add("Orange");
+        fruit.add("Strawberry");
+        fruit.add("Grape");
+
+        for(String food : fruit) {
+            if (food.contains("b") || food.contains("B")) {
+                System.out.println(food);
+            }
+        }
     }
 }
